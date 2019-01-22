76d4ae89f62a67b6b1db6b6aecbb428cc02b47f5
Jonathan Silverblood <gitlab@monsterbitar.se>
2019-01-22 14:06:43 +0100

    add account route and enable express to parse proxy headers.

--- server.js
+++ server.js
@@ -7,12 +7,16 @@ const app = express();
 // Configure the express application?
 app.use(express.json());
 
+// Ask express to parse proxy headers.
+app.enable('trust proxy')
+
 // Configure express to prettify json.
 app.set('json spaces', 2);
 
 // Create routes from separate files.
 app.use('/newblock', require('./routes/parser.js'));
 app.use('/lookup', require('./routes/lookup.js'));
+app.use('/account', require('./routes/metadata.js'));
 //app.use('/register', require('./routes/register.js'));
 //app.use('/statistics', require('./routes/statistics.js'));
 
