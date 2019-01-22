7f8de125a0e76e13c0af302522a85a256f7edfe5
Jonathan Silverblood <gitlab@monsterbitar.se>
2019-01-22 14:36:29 +0100

    restructure configuration file.

--- config.js
+++ config.js
@@ -1,20 +1,31 @@
 module.exports = 
 {
-	// Operational mode, determines what data to store and process.
-	mode: 'minimal',
+	//
+	server:
+	{
+		// Which port the server should listen for requests on.
+		port: 3001,
+
+		// Where to store the servers database file(s).
+		database: './database.db',
+
+		// What data should be cached by the server.
+		// 1: Minimal (Lookup transaction data and inclusion proofs on-demand)
+		// 2: Default (Store transaction data and inclusion proofs in database)
+		storage: 2,
+	},
 
 	// Location and credentials to connect to a fullnode RPC.
 	node:
 	{
+		// The address where we can reach a Bitcoin RPC service.
 		address: '127.0.0.1',
+
+		// The port on which the Bitcoin RPC service listens to.
 		port: '8332',
+
+		// The username and password for the Bitcoin RPC service.
 		user: '',
 		pass: ''
 	},
-
-	// Location where we will store the database.
-	storage:
-	{
-		filename: './database.db'
-	}
 }
