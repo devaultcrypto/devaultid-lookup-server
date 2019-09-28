f654db3197da8f535efe55e39f1c567da5acbb02
Pro X <pro@proteanx.com>
2019-09-28 17:05:42 -0400

    Revert "test with bch bitcore pkg"
    This reverts commit 31544343e8ab45a419d79ba92e81afe99bb8a4fe.

--- src/protocol.js
+++ src/protocol.js
@@ -4,8 +4,8 @@ module.exports = function(app)
 	//
 	app.locals.debug.struct('Initializing protocol module.');
 
-	// Enable the bitcore-lib-cash library functions.
-	const BitcoreCash = require('bitcore-lib-cash');
+	// Enable the bitcore-lib-dvt library functions.
+	const BitcoreCash = require('bitcore-lib-dvt');
 
 	// Define protocol constants.
 	app.locals.protocol =
